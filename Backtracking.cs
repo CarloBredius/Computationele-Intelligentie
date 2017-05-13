17d0947c493ca2805c6a141d47c291c3a5ef4e82
CarloBredius <c.bredius@live.nl>
2017-05-13 18:34:03 +0200

    Find numbers not to expand
    put in a list NotExpandNumbers

--- Backtracking.cs
+++ Backtracking.cs
@@ -4,17 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApplication2
+namespace BackTracking
 {
     class Program
     {
+        static int N;
         static void Main(string[] args)
         {
             string line = Console.ReadLine();
             string[] seperated = line.Split(' ');
-            int N = seperated.Count(); // een sudoku van N x N
+            N = seperated.Count(); // een sudoku van N x N
             int[,] puzzle = new int[N, N];
             int number;
+            List <int> NotExpandNumbers = new List<int>();
+
             int maxSteps = N * N; // maximaal aantal stappen is van een N x N puzzel is een diepte van N x N
 
             for (int i = 0; i < N; i++)
@@ -29,6 +32,7 @@ namespace ConsoleApplication2
             }
             int[,] root = puzzle;
 
+            NotExpandNumbers = CheckNoUse(puzzle, 1, 0, NotExpandNumbers); // find numbers to not check for
             // apply backtracking
 
             // Write solution
@@ -40,7 +44,7 @@ namespace ConsoleApplication2
                 }
                 Console.WriteLine();
             }
-            Console.ReadLine(); 
+            Console.ReadLine();
         }
 
         public void BackTrack(int[,] puzzle)
@@ -62,27 +66,169 @@ namespace ConsoleApplication2
             }
         }
 
-        public void CheckNoUse(int[,] puzzle, int x, int y, int[] numbers)
+        public static List<int> CheckNoUse(int[,] puzzle, int x, int y, List<int> numbers)
         {
-            CheckLine(puzzle, x, y, numbers);
-            CheckColumn(puzzle, x, y, numbers);
+            CheckLine(puzzle, x, numbers);
+            CheckColumn(puzzle, y, numbers);
             CheckBlock(puzzle, x, y, numbers);
+            return numbers;
         }
-        public void CheckLine(int[,] puzzle, int x, int y, int[] numbers)
+        public static void CheckLine(int[,] puzzle, int x, List<int> numbers)
         {
-
+            for (int i = 0; i < N; i++)
+            {
+                if (puzzle[x, i] != 0)
+                {
+                    numbers.Add(puzzle[x, i]);
+                }
+            }
         }
-        public void CheckColumn(int[,] puzzle, int x, int y, int[] numbers)
+        public static void CheckColumn(int[,] puzzle, int y, List<int> numbers)
         {
-
+            for (int i = 0; i < N; i++)
+            {
+                if (puzzle[i, y] != 0)
+                {
+                    numbers.Add(puzzle[i, y]);
+                }
+            }
         }
-        public void CheckBlock(int[,] puzzle, int x, int y, int[] numbers)
+        public static void CheckBlock(int[,] puzzle, int x, int y, List<int> numbers)
         {
+            int firstThird = ((int)Math.Sqrt(puzzle.Length) - 1) / 3;
+            int secondThird = ((int)Math.Sqrt(puzzle.Length) - 1) * 2 / 3;
+            int lastThird = puzzle.Length - 1;
 
+            if (0 <= x && x <= firstThird)   // first third of row
+            {
+                if (0 <= y && y <= firstThird)   // first third of column, block 1
+                {
+                    for (int i = 0; i <= firstThird; i++)
+                    {
+                        for (int j = 0; j <= firstThird; j++)
+                        {
+                            if (puzzle[i, j] != 0)
+                            {
+                                numbers.Add(puzzle[i, j]);
+                            }
+                        }
+                    }
+                }
+                else if (3 <= y && y <= 5)  // second third of collumn, block 2
+                {
+                    for (int i = firstThird; i <= secondThird; i++)
+                    {
+                        for (int j = 0; j <= firstThird; j++)
+                        {
+                            if (puzzle[i, j] != 0)
+                            {
+                                numbers.Add(puzzle[i, j]);
+                            }
+                        }
+                    }
+                }
+                else // block 3
+                {
+                    for (int i = secondThird; i <= lastThird; i++)
+                    {
+                        for (int j = 0; j <= firstThird; j++)
+                        {
+                            if (puzzle[i, j] != 0)
+                            {
+                                numbers.Add(puzzle[i, j]);
+                            }
+                        }
+                    }
+                }
+            }
+            else if (3 <= x && x <= 5) // second third of row
+            {
+                if (0 <= y && y <= 2)   // first third of column, block 4
+                {
+                    for (int i = 0; i <= firstThird; i++)
+                    {
+                        for (int j = firstThird; j <= secondThird; j++)
+                        {
+                            if (puzzle[i, j] != 0)
+                            {
+                                numbers.Add(puzzle[i, j]);
+                            }
+                        }
+                    }
+                }
+                else if (3 <= y && y <= 5)  // second third of collumn, block 5
+                {
+                    for (int i = firstThird; i <= secondThird; i++)
+                    {
+                        for (int j = firstThird; j <= secondThird; j++)
+                        {
+                            if (puzzle[i, j] != 0)
+                            {
+                                numbers.Add(puzzle[i, j]);
+                            }
+                        }
+                    }
+                }
+                else // block 6
+                {
+                    for (int i = secondThird; i <= lastThird; i++)
+                    {
+                        for (int j = firstThird; j <= secondThird; j++)
+                        {
+                            if (puzzle[i, j] != 0)
+                            {
+                                numbers.Add(puzzle[i, j]);
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if (0 <= y && y <= 2)   // first third of column, block 7
+                {
+                    for (int i = 0; i <= firstThird; i++)
+                    {
+                        for (int j = secondThird; j <= lastThird; j++)
+                        {
+                            if (puzzle[i, j] != 0)
+                            {
+                                numbers.Add(puzzle[i, j]);
+                            }
+                        }
+                    }
+                }
+                else if (3 <= y && y <= 5)  // second third of collumn, block 8
+                {
+                    for (int i = firstThird; i <= secondThird; i++)
+                    {
+                        for (int j = secondThird; j <= lastThird; j++)
+                        {
+                            if (puzzle[i, j] != 0)
+                            {
+                                numbers.Add(puzzle[i, j]);
+                            }
+                        }
+                    }
+                }
+                else // block 9
+                {
+                    for (int i = secondThird; i <= lastThird; i++)
+                    {
+                        for (int j = secondThird; j <= lastThird; j++)
+                        {
+                            if (puzzle[i, j] != 0)
+                            {
+                                numbers.Add(puzzle[i, j]);
+                            }
+                        }
+                    }
+                }
+            }
         }
         public bool Empty(int[,] puzzle)
         {
-            foreach(int number in puzzle)
+            foreach (int number in puzzle)
             {
                 if (number != 0)
                 {
