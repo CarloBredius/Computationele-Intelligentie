4ff06cd6e9b972a91bd1f7c63dd44456edadec0b
CarloBredius <c.bredius@live.nl>
2017-05-16 17:18:37 +0200

    Naar 2D in array verandert
    Wanneer vooraf in de code ingevuld: 6,5 seconde (zelfde met int[][]).
    Wanneer afgelezen van de console: 25 seconde. (21 seconde met int[][]).
    navragen waarom

--- Backtracking.cs
+++ Backtracking.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +6,11 @@ namespace ConsoleApp2
 {
     class Program
     {
+        // N is used for size of Sudoku grid. Size will be NxN
+        int N = 9;
         /* Takes a partially filled-in grid and attempts to assign values to
-          all unassigned locations in such a way to meet the requirements
-          for Sudoku solution (non-duplication across rows, columns, and boxes) */
+                all unassigned locations in such a way to meet the requirements
+                for Sudoku solution (non-duplication across rows, columns, and boxes) */
         static bool SolveSudoku(int[,] grid, int N, int boxSize)
         {
             int[] position;
@@ -27,31 +27,31 @@ namespace ConsoleApp2
                 if (isSafe(grid, N, boxSize, row, col, num))
                 {
                     // make tentative assignment
-                    grid[row, col] = num;
-
+                    grid[row,col] = num;
                     // return, if success, yay!
                     if (SolveSudoku(grid, N, boxSize))
                         return true;
-
                     // failure, unmake & try again
-                    grid[row, col] = 0;
+                    grid[row,col] = 0;
                 }
+
             }
             return false; // this triggers backtracking
         }
 
         /* Searches the grid to find an entry that is still unassigned. If
-           found, the reference parameters row, col will be set the location
-           that is unassigned, and true is returned. If no unassigned entries
-           remain, false is returned. */
+         found, the reference parameters row, col will be set the location
+         that is unassigned, and true is returned. If no unassigned entries
+         remain, false is returned. */
         static int[] FindUnassignedLocation(int[,] grid, int N)
         {
+
             int[] point = new int[2];
             point[0] = -1;
             point[1] = -1;
             for (int row = 0; row < N; row++)
                 for (int col = 0; col < N; col++)
-                    if (grid[row, col] == 0)
+                    if (grid[row,col] == 0)
                     {
                         point[0] = row;
                         point[1] = col;
@@ -59,23 +59,21 @@ namespace ConsoleApp2
                     }
             return point;
         }
-
         /* Returns a boolean which indicates whether any assigned entry
-           in the specified row matches the given number. */
+         in the specified row matches the given number. */
         static bool UsedInRow(int[,] grid, int N, int row, int num)
         {
             for (int col = 0; col < N; col++)
-                if (grid[row, col] == num)
+                if (grid[row,col] == num)
                     return true;
             return false;
         }
-
         /* Returns a boolean which indicates whether any assigned entry
            in the specified column matches the given number. */
         static bool UsedInCol(int[,] grid, int N, int col, int num)
         {
             for (int row = 0; row < N; row++)
-                if (grid[row, col] == num)
+                if (grid[row,col] == num)
                     return true;
             return false;
         }
@@ -87,7 +85,7 @@ namespace ConsoleApp2
 
             for (int row = 0; row < boxSize; row++)
                 for (int col = 0; col < boxSize; col++)
-                    if (grid[row + boxStartRow, col + boxStartCol] == num)
+                    if (grid[row + boxStartRow,col + boxStartCol] == num)
                         return true;
             return false;
         }
@@ -97,13 +95,11 @@ namespace ConsoleApp2
         static bool isSafe(int[,] grid, int N, int boxSize, int row, int col, int num)
         {
             /* Check if 'num' is not already placed in current row,
-               current column and current NxN box */
-
+        current column and current NxN box */
             return !UsedInRow(grid, N, row, num) &&
                    !UsedInCol(grid, N, col, num) &&
                    !UsedInBox(grid, boxSize, row - row % boxSize, col - col % boxSize, num);
         }
-
         /* A utility function to print grid  */
         static void printGrid(int[,] grid, int N)
         {
@@ -111,39 +107,52 @@ namespace ConsoleApp2
             {
                 for (int col = 0; col < N; col++)
                 {
-                    Console.Write(grid[row, col].ToString() + ' ');
+                    Console.Write(grid[row,col].ToString() + ' ');
                 }
                 Console.WriteLine();
             }
             Console.ReadLine();
         }
         static int[,] readGrid()
-        {
-            int N;
-            string line = Console.ReadLine();
-            string[] seperated = line.Split(' ');
-            N = seperated.Count(); // een sudoku van N x N
-            int[,] puzzle = new int[N, N];
-            int number;
+          {
+              int N;
+              string line = Console.ReadLine();
+              string[] seperated = line.Split(' ');
+              N = seperated.Length; // een sudoku van N x N
+              int[,] puzzle = new int[N,N];
+              int number;
 
             for (int i = 0; i < N; i++)
             {
-                for (int j = 0; j < N; j++)
-                {
-                    number = Convert.ToInt32(seperated[j]);
-                    puzzle[i, j] = number;
-                }
-                line = Console.ReadLine();
-                seperated = line.Split(' ');
-            }
-            return puzzle;
-        }
+                //puzzle[i] = new int[N];
+                  for (int j = 0; j < N; j++)
+                  {
+                      number = Convert.ToInt32(seperated[j]);
+                      puzzle[i,j] = number;
+                  }
+                  line = Console.ReadLine();
+                  seperated = line.Split(' ');
+              }
+              return puzzle;
+          }
         static void Main(string[] args)
         {
             int[,] grid = readGrid();
-            int N = Convert.ToInt32(Math.Sqrt(grid.Length));
+
+            //int[,] grid = new int[9, 9]{
+            //    { 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            //    { 0, 0, 0, 0, 0, 3, 0, 8, 5 },
+            //    { 0, 0, 1, 0, 2, 0, 0, 0, 0 },
+            //    { 0, 0, 0, 5, 0, 7, 0, 0, 0 },
+            //    { 0, 0, 4, 0, 0, 0, 1, 0, 0 },
+            //    { 0, 9, 0, 0, 0, 0, 0, 0, 0 },
+            //    { 0, 0, 0, 5, 0, 7, 0, 0, 0 },
+            //    { 5, 0, 0, 0, 0, 0, 0, 7, 3 },
+            //    { 0, 0, 2, 0, 1, 0, 0, 0, 0 }
+            //    };
+            int N = (int)Math.Sqrt(grid.Length);
             int boxSize = Convert.ToInt32(Math.Sqrt(N));
-            //printGrid(grid, N);
+
             if (SolveSudoku(grid, N, boxSize) == true)
                 printGrid(grid, N);
             else
