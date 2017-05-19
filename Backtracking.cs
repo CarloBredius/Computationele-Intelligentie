5f25f4de83bd7c51882f4628e516cc91b4d7d7cf
JoostSomers <j.somers1@students.uu.nl>
2017-05-20 00:22:37 +0200

    hele opdracht af.
    geef eerst 1 2 of 3 en druk dan op enter voor je de sudoku invoert. 1 is
    van linksboven naar rechtsonder, 2 is andersom, en 3 is op oplopende
    volgorde van aantal mogelijkheden

--- Backtracking.cs
+++ Backtracking.cs
@@ -1,23 +1,25 @@
 ﻿using System;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp2
 {
     class Program
     {
-        // N is used for size of Sudoku grid. Size will be NxN
-        int N = 9;
         /* Takes a partially filled-in grid and attempts to assign values to
                 all unassigned locations in such a way to meet the requirements
                 for Sudoku solution (non-duplication across rows, columns, and boxes) */
-        static bool SolveSudoku(int[,] grid, int N, int boxSize)
+        static bool SolveSudoku(int[,] grid, int N, int boxSize, string algorithm, List<int[]> searchOrder)
         {
             int[] position;
             // If there is no unassigned location, we are done
-            position = FindUnassignedLocation(grid, N);
+
+            position = FindUnassignedLocation(grid, N, algorithm, searchOrder);
             if (position[0] == -1)
                 return true; // success!
+
             int row = position[0];
             int col = position[1];
             // consider digits 1 to N
@@ -27,12 +29,14 @@ namespace ConsoleApp2
                 if (isSafe(grid, N, boxSize, row, col, num))
                 {
                     // make tentative assignment
-                    grid[row,col] = num;
+                    grid[row, col] = num;
+                    if (algorithm == "3") { searchOrder.RemoveAt(0); }
                     // return, if success, yay!
-                    if (SolveSudoku(grid, N, boxSize))
+                    if (SolveSudoku(grid, N, boxSize, algorithm, searchOrder))
                         return true;
                     // failure, unmake & try again
-                    grid[row,col] = 0;
+                    grid[row, col] = 0;
+                    if (algorithm == "3") { searchOrder.Insert(0, position); }
                 }
 
             }
@@ -43,20 +47,41 @@ namespace ConsoleApp2
          found, the reference parameters row, col will be set the location
          that is unassigned, and true is returned. If no unassigned entries
          remain, false is returned. */
-        static int[] FindUnassignedLocation(int[,] grid, int N)
+        static int[] FindUnassignedLocation(int[,] grid, int N, string algorithm, List<int[]> searchOrder)
         {
 
             int[] point = new int[2];
             point[0] = -1;
             point[1] = -1;
-            for (int row = 0; row < N; row++)
-                for (int col = 0; col < N; col++)
-                    if (grid[row,col] == 0)
-                    {
-                        point[0] = row;
-                        point[1] = col;
-                        return point;
-                    }
+            if (algorithm == "1")
+            {
+                for (int row = 0; row < N; row++)
+                    for (int col = 0; col < N; col++)
+                        if (grid[row, col] == 0)
+                        {
+                            point[0] = row;
+                            point[1] = col;
+                            return point;
+                        }
+            }
+            else if (algorithm == "2")
+            {
+                for (int row = N - 1; row >= 0; row--)
+                    for (int col = N - 1; col >= 0; col--)
+                        if (grid[row, col] == 0)
+                        {
+                            point[0] = row;
+                            point[1] = col;
+                            return point;
+                        }
+            }
+            else if (algorithm == "3")
+            {
+                if (searchOrder.Count() > 0)
+                {
+                    point = searchOrder.First();
+                }
+            }
             return point;
         }
         /* Returns a boolean which indicates whether any assigned entry
@@ -64,7 +89,7 @@ namespace ConsoleApp2
         static bool UsedInRow(int[,] grid, int N, int row, int num)
         {
             for (int col = 0; col < N; col++)
-                if (grid[row,col] == num)
+                if (grid[row, col] == num)
                     return true;
             return false;
         }
@@ -73,7 +98,7 @@ namespace ConsoleApp2
         static bool UsedInCol(int[,] grid, int N, int col, int num)
         {
             for (int row = 0; row < N; row++)
-                if (grid[row,col] == num)
+                if (grid[row, col] == num)
                     return true;
             return false;
         }
@@ -85,7 +110,7 @@ namespace ConsoleApp2
 
             for (int row = 0; row < boxSize; row++)
                 for (int col = 0; col < boxSize; col++)
-                    if (grid[row + boxStartRow,col + boxStartCol] == num)
+                    if (grid[row + boxStartRow, col + boxStartCol] == num)
                         return true;
             return false;
         }
@@ -101,42 +126,94 @@ namespace ConsoleApp2
                    !UsedInBox(grid, boxSize, row - row % boxSize, col - col % boxSize, num);
         }
         /* A utility function to print grid  */
+
+        static List<int[]> searchOrder(int[,] grid, int N, int boxSize)
+        {
+            List<int[]> positions = new List<int[]>();
+            List<int> heuristicValues = new List<int>();
+
+
+            for (int row = 0; row < N; row++)
+            {
+                for (int col = 0; col < N; col++)
+                {
+                    if (grid[row, col] == 0)
+                    {
+                        int[] position = new int[2] { row, col };
+                        heuristicValues.Add(getHeuristicValue(grid, N, boxSize, row, col));
+                        positions.Add(position);
+                    }
+                }
+            }
+            positions = positions
+              .Zip(heuristicValues, (p, h) => new { positions = p, heuristicValues = h })
+              .OrderBy(v => v.heuristicValues)
+              .Select(v => v.positions)
+              .ToList();
+
+            //positionsWithHeuristicValue.OrderBy(item => item[0]);
+            //List<int[]> positions = new List<int[]>();
+            //foreach (int[] position in positionsWithHeuristicValue) { positions.Add(new int[2]{ position[1], position[2]}); }
+            return positions;
+        }
+
+        static int getHeuristicValue(int[,] grid, int N, int boxSize, int row, int col)
+        {
+            int heuristicValue = 0;
+            for (int num = 1; num <= N; num++)
+            {
+                if (isSafe(grid, N, boxSize, row, col, num))
+                { heuristicValue++; }
+            }
+            return heuristicValue;
+        }
         static void printGrid(int[,] grid, int N)
         {
             for (int row = 0; row < N; row++)
             {
                 for (int col = 0; col < N; col++)
                 {
-                    Console.Write(grid[row,col].ToString() + ' ');
+                    Console.Write(grid[row, col].ToString() + ' ');
                 }
                 Console.WriteLine();
             }
             Console.ReadLine();
         }
         static int[,] readGrid()
-          {
-              int N;
-              string line = Console.ReadLine();
-              string[] seperated = line.Split(' ');
-              N = seperated.Length; // een sudoku van N x N
-              int[,] puzzle = new int[N,N];
-              int number;
+        {
+            int N;
+            string line = Console.ReadLine();
+            string[] seperated = line.Split(' ');
+            N = seperated.Length; // een sudoku van N x N
+            int[,] puzzle = new int[N, N];
+            int number;
 
             for (int i = 0; i < N; i++)
             {
                 //puzzle[i] = new int[N];
-                  for (int j = 0; j < N; j++)
-                  {
-                      number = Convert.ToInt32(seperated[j]);
-                      puzzle[i,j] = number;
-                  }
-                  line = Console.ReadLine();
-                  seperated = line.Split(' ');
-              }
-              return puzzle;
-          }
+                for (int j = 0; j < N; j++)
+                {
+                    number = Convert.ToInt32(seperated[j]);
+                    puzzle[i, j] = number;
+                }
+                line = Console.ReadLine();
+                seperated = line.Split(' ');
+            }
+            return puzzle;
+        }
         static void Main(string[] args)
         {
+            string algorithm = "0";
+            do
+            {
+                Console.WriteLine("choose 1, 2, or 3");
+                algorithm = Console.ReadLine();
+                Console.WriteLine(algorithm);
+            } while (algorithm != "1" && algorithm != "2" && algorithm != "3");
+
+            Console.WriteLine("algorithm " + algorithm + " accepted");
+            Console.WriteLine("enter a NxN sudoku with N lines of N numbers with white space between them");
+
             int[,] grid = readGrid();
 
             //int[,] grid = new int[9, 9]{
@@ -152,8 +229,14 @@ namespace ConsoleApp2
             //    };
             int N = (int)Math.Sqrt(grid.Length);
             int boxSize = Convert.ToInt32(Math.Sqrt(N));
+            List<int[]> searchList = new List<int[]>();
+            if (algorithm == "3")
+            {
+                searchList = searchOrder(grid, N, boxSize);
+            }
 
-            if (SolveSudoku(grid, N, boxSize) == true)
+
+            if (SolveSudoku(grid, N, boxSize, algorithm, searchList) == true)
                 printGrid(grid, N);
             else
                 Console.WriteLine("No solution exists");
