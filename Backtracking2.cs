08d2d9c107c7a1e136319f5aebdb14ad764d1b81
JoostSomers <j.somers1@students.uu.nl>
2017-06-06 13:31:06 +0200

    bhjbvhjk

--- Backtracking2.cs
+++ Backtracking2.cs
@@ -4,184 +4,110 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace ConsoleApp2
+namespace ConsoleApp1
 {
-    // Elke stap ded listofarrays updaten, als er daarna een lege lijst ontstaat --> backtrack, expandeer de boom met de lijst die het minst lang is.
     class Program
     {
         static int recursiveSteps = 0;
         /* Takes a partially filled-in grid and attempts to assign values to
                 all unassigned locations in such a way to meet the requirements
                 for Sudoku solution (non-duplication across rows, columns, and boxes) */
-        static bool SolveSudoku(int[,] grid, int N, int boxSize, string algorithm, List<int[]> searchOrder)
+        static bool SolveSudoku(List<int>[,] grid, int N, int boxSize)
         {
-            // keep track of the amount of recusrive steps
+            // keep track of the amount of recursive steps
             recursiveSteps++;
 
-            int[] position;            
-
-            // If there is no unassigned location, we are done
-            position = FindUnassignedLocation(grid, N, algorithm, searchOrder);
-            if (position[0] == -1)
-                return true; // success!
-
-            int row = position[0];
-            int col = position[1];
-            // consider digits 1 to N
-            for (int num = 1; num <= N; num++)
-            {
-                // if looks promising
-                if (isSafe(grid, N, boxSize, row, col, num))
+            int row, col;
+            //find smallest domain that doesn't contain 1 value
+            int smallest = N;
+            int l = N;
+            for (int x = 0; x < N; x++)
+                for (int y = 0; y < N; y++)
                 {
-                    // make tentative assignment
-                    grid[row, col] = num;
-                    if (algorithm == "3") { searchOrder.RemoveAt(0); }
+                    l = grid[x, y].Count;
+                    if (l < smallest && l > 1)
+                    {
+                        smallest = l;
+                        row = y;
+                        col = x;
+                    }
+                }
+            // If there is none, we are done
+            return true; // success!
+                         //else get the position of this domain and the domain
+
+            //loop through domain
+            foreach (int num in grid[row, col])
+            {
+                List<int>[,] copy;
+                copy = grid;
+                // check if a domain collapses to zero 
+                if (updateDomains(grid, N, boxSize, row, col, num))
+                {
                     // return, if success, yay!
-                    if (SolveSudoku(grid, N, boxSize, algorithm, searchOrder))
+                    if (SolveSudoku(grid, N, boxSize))
                         return true;
                     // failure, unmake & try again
-                    grid[row, col] = 0;
-                    if (algorithm == "3") { searchOrder.Insert(0, position); }
+                    grid = copy;
+                    //get back to the old copy
+
                 }
 
             }
             return false; // this triggers backtracking
         }
 
-        /* Searches the grid to find an entry that is still unassigned. If
-         found, the reference parameters row, col will be set the location
-         that is unassigned, and true is returned. If no unassigned entries
-         remain, false is returned. */
-        static int[] FindUnassignedLocation(int[,] grid, int N, string algorithm, List<int[]> searchOrder)
-        {
-            int[] point = new int[2];
-            point[0] = -1;
-            point[1] = -1;
-            if (algorithm == "1")
-            {
-                for (int row = 0; row < N; row++)
-                    for (int col = 0; col < N; col++)
-                        if (grid[row, col] == 0)
-                        {
-                            point[0] = row;
-                            point[1] = col;
-                            return point;
-                        }
-            }
-            else if (algorithm == "2")
-            {
-                for (int row = N - 1; row >= 0; row--)
-                    for (int col = N - 1; col >= 0; col--)
-                        if (grid[row, col] == 0)
-                        {
-                            point[0] = row;
-                            point[1] = col;
-                            return point;
-                        }
-            }
-            else if (algorithm == "3")
-            {
-                if (searchOrder.Count() > 0)
-                {
-                    point = searchOrder.First();
-                }
-            }
-            return point;
-        }
+
         /* Returns a boolean which indicates whether any assigned entry
          in the specified row matches the given number. */
-        static bool UsedInRow(int[,] grid, int N, int row, int num)
+        static bool notAllowedInRow(List<int>[,] grid, int N, int row, int num)
         {
             for (int col = 0; col < N; col++)
-                if (grid[row, col] == num)
+            {
+                if (grid[row, col] == new List<int> { num })
                     return true;
+                grid[row, col].Remove(num);
+            }
             return false;
         }
         /* Returns a boolean which indicates whether any assigned entry
            in the specified column matches the given number. */
-        static bool UsedInCol(int[,] grid, int N, int col, int num)
+        static bool notAllowedInCol(List<int>[,] grid, int N, int col, int num)
         {
             for (int row = 0; row < N; row++)
-                if (grid[row, col] == num)
+            {
+                if (grid[row, col] == new List<int> { num })
                     return true;
+                grid[row, col].Remove(num);
+            }
             return false;
         }
 
         /* Returns a boolean which indicates whether any assigned entry
            within the specified 3x3 box matches the given number. */
-        static bool UsedInBox(int[,] grid, int boxSize, int boxStartRow, int boxStartCol, int num)
+        static bool notAllowedInBox(List<int>[,] grid, int boxSize, int boxStartRow, int boxStartCol, int num)
         {
-
             for (int row = 0; row < boxSize; row++)
                 for (int col = 0; col < boxSize; col++)
-                    if (grid[row + boxStartRow, col + boxStartCol] == num)
+                {
+                    if (grid[row + boxStartRow, col + boxStartCol] == new List<int> { num })
                         return true;
+                    grid[row + boxStartRow, col + boxStartCol].Remove(num);
+                }
             return false;
         }
 
-        /* Returns a boolean which indicates whether it will be legal to assign
-           num to the given row,col location. */
-        static bool isSafe(int[,] grid, int N, int boxSize, int row, int col, int num)
+        static bool updateDomains(List<int>[,] grid, int N, int boxSize, int row, int col, int num)
         {
             /* Check if 'num' is not already placed in current row,
         current column and current NxN box */
-            return !UsedInRow(grid, N, row, num) &&
-                   !UsedInCol(grid, N, col, num) &&
-                   !UsedInBox(grid, boxSize, row - row % boxSize, col - col % boxSize, num);
-        }
-        /* A utility function to print grid  */
-        static void UpdateList(int[,] grid, int N, int boxSize, int row, int col, int num, List<int>[,] listArrays)
-        {
-            /* Check if 'num' is not already placed in current row,
-        current column and current NxN box */
-            if (!UsedInRow(grid, N, row, num) &&
-                   !UsedInCol(grid, N, col, num) &&
-                   !UsedInBox(grid, boxSize, row - row % boxSize, col - col % boxSize, num))
-            {
-                listArrays[row, col].Add(num);
-            }
+            return !notAllowedInRow(grid, N, row, num) &&
+                   !notAllowedInCol(grid, N, col, num) &&
+                   !notAllowedInBox(grid, boxSize, row - row % boxSize, col - col % boxSize, num);
         }
 
-        static List<int[]> searchOrder(int[,] grid, int N, int boxSize)
-        {
-            List<int[]> positions = new List<int[]>();
-            List<int> heuristicValues = new List<int>();
 
 
-            for (int row = 0; row < N; row++)
-            {
-                for (int col = 0; col < N; col++)
-                {
-                    if (grid[row, col] == 0)
-                    {
-                        int[] position = new int[2] { row, col };
-                        heuristicValues.Add(getHeuristicValue(grid, N, boxSize, row, col));
-                        positions.Add(position);
-                    }
-                }
-            }
-            positions = positions
-              .Zip(heuristicValues, (p, h) => new { positions = p, heuristicValues = h })
-              .OrderBy(v => v.heuristicValues)
-              .Select(v => v.positions)
-              .ToList();
-
-            //positionsWithHeuristicValue.OrderBy(item => item[0]);
-            //List<int[]> positions = new List<int[]>();
-            //foreach (int[] position in positionsWithHeuristicValue) { positions.Add(new int[2]{ position[1], position[2]}); }
-            return positions;
-        }
-
-        static int getHeuristicValue(int[,] grid, int N, int boxSize, int row, int col)
-        {
-            int heuristicValue = 0;
-            for (int num = 1; num <= N; num++)
-            {
-                if (isSafe(grid, N, boxSize, row, col, num))
-                { heuristicValue++; }
-            }
-            return heuristicValue;
-        }
         static void printGrid(int[,] grid, int N)
         {
             for (int row = 0; row < N; row++)
@@ -215,54 +141,56 @@ namespace ConsoleApp2
             }
             return puzzle;
         }
+
+        static List<int>[,] gridToDomainGrid(int[,] grid, int N, int boxSize)
+        {
+            List<int>[,] domainGrid = new List<int>[N, N];
+            List<int> domain = Enumerable.Range(1, N).ToList();
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    domainGrid[i, j] = domain;
+                }
+            }
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (grid[i, j] != 0)
+                    {
+                        domainGrid[i, j] = new List<int> { grid[i, j] };
+                        updateDomains(domainGrid, N, boxSize, i, j, grid[i, j]);
+                    }
+
+                }
+            }
+            return domainGrid;
+        }
         static void Main(string[] args)
         {
-            string algorithm;
-            do
-            {
-                Console.WriteLine("choose 1, 2, or 3");
-                algorithm = Console.ReadLine();
-                Console.WriteLine(algorithm);
-            } while (algorithm != "1" && algorithm != "2" && algorithm != "3");
-
-            Console.WriteLine("algorithm " + algorithm + " accepted");
             Console.WriteLine("enter a NxN sudoku with N lines of N numbers with white space between them");
 
             int[,] grid = readGrid();
 
             int N = (int)Math.Sqrt(grid.Length);
             int boxSize = Convert.ToInt32(Math.Sqrt(N));
-            List<int[]> searchList = new List<int[]>();
 
             // Keep track of an NxN array of lists, containing the numbers possible at that position
-            List<int>[,] listArrays = new List<int>[9, 9];
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    listArrays[i, j] = new List<int>();
-                    for (int num = 1; num <= N; num++)
-                    {
-                        UpdateList(grid, N, boxSize, i, j, num, listArrays);
-                    }
-                }
-            }
-            Console.ReadLine();
+            List<int>[,] domainGrid = new List<int>[N, N];
+            domainGrid = gridToDomainGrid(grid, N, boxSize);
 
-            if (algorithm == "3")
-            {
-                searchList = searchOrder(grid, N, boxSize);
-            }
+
 
             // Keep track of time
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
-            if (SolveSudoku(grid, N, boxSize, algorithm, searchList) == true)
+            if (SolveSudoku(domainGrid, N, boxSize) == true)
             {
                 printGrid(grid, N);
                 watch.Stop();
                 var elapsedMs = watch.ElapsedMilliseconds;
-                Console.WriteLine("Elapsed time: " + elapsedMs/1000 + '.' + (elapsedMs - (elapsedMs / 1000)) + " seconds");
+                Console.WriteLine("Elapsed time: " + elapsedMs / 1000 + '.' + (elapsedMs - (elapsedMs / 1000)) + " seconds");
                 Console.WriteLine("Recursive steps: " + recursiveSteps);
                 Console.ReadLine();
             }
@@ -271,3 +199,4 @@ namespace ConsoleApp2
         }
     }
 }
+
